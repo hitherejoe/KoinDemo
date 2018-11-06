dc23c410e9bd530192f7d84976e9149dfd87ddc1
Joe Birch <hitherejoebirch@googlemail.com>
2018-11-06 09:59:50 +0000

    Add missing code

--- app/src/main/java/co/joebirch/koindemo/modules.kt
+++ app/src/main/java/co/joebirch/koindemo/modules.kt
@@ -1,9 +1,11 @@
 package co.joebirch.koindemo
 
+import co.joebirch.koindemo.data.DataRepositoryImpl
 import com.google.gson.Gson
 import org.koin.dsl.module.module
 
 val applicationModule = module {
     single { Gson() }
     factory { CurrenciesAdapter() }
+    factory { DataRepositoryImpl() }
 }
