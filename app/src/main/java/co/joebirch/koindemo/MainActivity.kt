82987eeb59f22e63bb622cfc7c052de1f3492ca7
Joe Birch <hitherejoebirch@googlemail.com>
2018-11-06 09:31:04 +0000

    Fix activity

--- app/src/main/java/co/joebirch/koindemo/MainActivity.kt
+++ app/src/main/java/co/joebirch/koindemo/MainActivity.kt
@@ -1,39 +1,20 @@
 package co.joebirch.koindemo
 
-import android.arch.lifecycle.Observer
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import co.joebirch.koindemo.presentation.CurrenciesViewModel
 import kotlinx.android.synthetic.main.activity_main.recycler_currencies
 import org.koin.android.ext.android.inject
-import org.koin.android.scope.ext.android.bindScope
-import org.koin.android.scope.ext.android.getOrCreateScope
-import org.koin.android.viewmodel.ext.android.viewModel
-import org.koin.core.parameter.parametersOf
 
 class MainActivity : AppCompatActivity() {
 
     val currenciesAdapter: CurrenciesAdapter by inject()
-    val currenciesViewModel: CurrenciesViewModel by viewModel {
-        val currenciesJson = resources.openRawResource(R.raw.currencies)
-                .bufferedReader().use { it.readText() }
-        parametersOf(currenciesJson)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bindScope(getOrCreateScope("browse"))
-
         setupCurrenciesRecycler()
-
-        currenciesViewModel.observeCurrencies().observe(this, Observer {
-            it?.let { currenciesAdapter.currencies = it }
-        })
-
-        currenciesViewModel.retrieveCurrencies()
     }
 
     private fun setupCurrenciesRecycler() {
