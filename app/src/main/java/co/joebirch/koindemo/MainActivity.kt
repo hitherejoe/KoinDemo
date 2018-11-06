97fff74efddc2f6a616a84732745f2565f931ab9
Joe Birch <hitherejoebirch@googlemail.com>
2018-11-06 09:35:19 +0000

    Add repository injection

--- app/src/main/java/co/joebirch/koindemo/MainActivity.kt
+++ app/src/main/java/co/joebirch/koindemo/MainActivity.kt
@@ -3,18 +3,25 @@ package co.joebirch.koindemo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import co.joebirch.koindemo.data.DataRepositoryImpl
 import kotlinx.android.synthetic.main.activity_main.recycler_currencies
 import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
 
     val currenciesAdapter: CurrenciesAdapter by inject()
+    val dataRepository: DataRepositoryImpl by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setupCurrenciesRecycler()
+
+        val currenciesJson = resources.openRawResource(R.raw.currencies)
+                .bufferedReader().use { it.readText() }
+        val items = dataRepository.getCurrencies(currenciesJson)
+        currenciesAdapter.currencies = items
     }
 
     private fun setupCurrenciesRecycler() {
