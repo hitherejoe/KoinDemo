213d6a54b0859bf15252d262eedfa9a701b49c02
Joe Birch <hitherejoebirch@googlemail.com>
2018-11-06 09:09:51 +0000

    Example code for setup step

--- app/src/main/java/co/joebirch/koindemo/data/DataRepository.kt
+++
@@ -1,9 +0,0 @@
-package co.joebirch.koindemo.data
-
-import co.joebirch.koindemo.model.Currency
-
-interface DataRepository {
-
-    fun getCurrencies(jsonString: String): List<Currency>
-
-}
--- app/src/main/java/co/joebirch/koindemo/model/Currency.kt
+++
@@ -1,6 +0,0 @@
-package co.joebirch.koindemo.model
-
-data class Currency(val id: Int,
-                    val name: String,
-                    val symbol: String,
-                    val slug: String)
