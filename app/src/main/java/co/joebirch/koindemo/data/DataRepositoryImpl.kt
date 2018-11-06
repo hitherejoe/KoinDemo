71de5b6da16847a1d7297171c1c20ff1b0365dea
Joe Birch <hitherejoebirch@googlemail.com>
2018-11-06 09:19:53 +0000

    Example code for singletons

--- app/src/main/java/co/joebirch/koindemo/data/DataRepositoryImpl.kt
+++ app/src/main/java/co/joebirch/koindemo/data/DataRepositoryImpl.kt
@@ -1,11 +1,12 @@
 package co.joebirch.koindemo.data
 
 import co.joebirch.koindemo.model.Currency
+import com.google.gson.Gson
 
-open class DataRepositoryImpl {
+open class DataRepositoryImpl(private val gson: Gson) {
 
     fun getCurrencies(jsonString: String): List<Currency> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return gson.fromJson(jsonString, Array<Currency>::class.java).toList()
     }
 
 }
